622d1302c3e83810c372cbdc71cefae51bf92524
Alex Watts <alexjwatts83@gmail.com>
2021-08-05 07:07:02 +1000

    Added FluentNHibernateHelper

---
+++ src/NHibernateDemo.Infrastructure/FluentNHibernateHelper.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using NHibernateDemo.Infrastructure.Entities;
+
+namespace NHibernateDemo.Infrastructure
+{
+    public static class FluentNHibernateHelper
+    {
+        public static ISession OpenSession()
+        {
+            string connectionString = "Write your database connection string here";
+
+            ISessionFactory sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql7.ConnectionString(connectionString).ShowSql())
+                .Mappings(m =>
+                          m.FluentMappings
+                              .AddFromAssemblyOf<Product>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                .Create(false, false))
+                .BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
