6afc64c97d53be66ab1513c2e00ed29c2aadebfb
Alex Watts <alexjwatts83@gmail.com>
2021-08-05 07:15:12 +1000

    Added dapper for ensuring the db

--- src/NHibernateDemo.Infrastructure/PersistenceDbMigrations.cs
+++ src/NHibernateDemo.Infrastructure/PersistenceDbMigrations.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using Microsoft.Extensions.Logging;
 
 namespace NHibernateDemo.Infrastructure
 {
@@ -7,14 +10,14 @@ namespace NHibernateDemo.Infrastructure
     {
         public static void EnsureDatabase(string connectionString, string name)
         {
-            //var parameters = new DynamicParameters();
-            //parameters.Add("name", name);
-            //using var connection = new SqlConnection(connectionString);
-            //var records = connection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
-            //if (!records.Any())
-            //{
-            //    connection.Execute($"CREATE DATABASE {name}");
-            //}
+            var parameters = new DynamicParameters();
+            parameters.Add("name", name);
+            using var connection = new SqlConnection(connectionString);
+            var records = connection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+            if (!records.Any())
+            {
+                connection.Execute($"CREATE DATABASE {name}");
+            }
         }
 
         public static void UpdateDatabase(string connectionString, ILogger logger)
